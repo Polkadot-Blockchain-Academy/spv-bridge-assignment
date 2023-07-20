d9a39c61a3c1b852087b409348fff48d47579fcd
Joshy Orndorff <admin@joshyorndorff.com>
2023-07-20 10:00:57 -0700

    refactor tests and sketch first one

---
+++ test/SpvBridge.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/SpvBridge.sol";
+
+contract SpvBridgeTest is Test {
+    SpvBridge public bridge;
+    uint threshold = 10_000_000_000;
+    uint relay_fee = 1_000;
+    Header genesis;
+
+    event HeaderSubmitted(uint256 block_hash, uint256 block_height, address submitter);
+
+    function make_child(Header memory parent) public view returns (Header memory) {
+        uint256 parent_hash = uint(keccak256(abi.encode(parent)));
+        Header memory child = Header ({
+            height: parent.height + 1,
+            parent: parent_hash,
+            storage_root: 0,
+            transactions_root: 0,
+            pow_nonce: 0
+        });
+
+        while (uint(keccak256(abi.encode(child))) >= threshold) {
+            child.pow_nonce = child.pow_nonce + 1;
+        }
+
+        return child;
+    }
+
+    function setUp() public {
+
+        genesis = Header({
+            height: 100,
+            parent: 0,
+            storage_root: 0,
+            transactions_root: 0,
+            // The initial block is not checked; not even its pow seal;
+            // We put a non-zero nonce here to make sure this block
+            // isn't the default block.
+            pow_nonce: 1
+        });
+
+        bridge = new SpvBridge(genesis, threshold, relay_fee);
+    }
+
+    function testSubmitExtendLongestChain() public {
+        Header memory child = make_child(genesis);
+
+        bridge.submit_new_header(child);
+        uint256 child_hash = uint(keccak256(abi.encode(child)));
+
+        vm.expectEmit();
+        emit HeaderSubmitted(child_hash, 1, 0x0000000000000000000000000000000000000000);
+    }
+}
+
+// Test braindump
+
+// constructor happy path
+// submit to extend longest chain
+// submit to add side chain
+// submit to cause re-org
+// Tx verification happy path
+// Tx verification fail path
+// State verification happy path
+// State verification one fail path
